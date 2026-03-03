2930323166989932bbfba6a9c0f1bb73a8db42e4
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-03 15:44:34 +0500

    Использование агрегатной функции AVG

--- File1.py
+++ File1.py
@@ -3,8 +3,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT SUM(age) FROM Users')
-total_age = cursor.fetchone()[0]
-print('Общая сумма возрастов пользователей:', total_age)
+cursor.execute('SELECT AVG(age) FROM Users')
+average_age = cursor.fetchone()[0]
+print('Средний возраст пользователей:', average_age)
     
 connection.close()
