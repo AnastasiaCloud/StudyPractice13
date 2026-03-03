d4393da23d811d5b509519aa18ef36257dd0b97e
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-03 14:45:11 +0500

    Выполнение запроса

--- File1.py
+++ File1.py
@@ -3,22 +3,11 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Users (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER)
-''')
-
-cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
-
-cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
-
-cursor.execute('DELETE FROM Users WHERE username = ?', ('newuser',))
-
 cursor.execute('SELECT * FROM Users')
-print(cursor.fetchall())
+users = cursor.fetchall()
 
-connection.commit()
+for user in users:
+    print(user)
+
+
 connection.close()
