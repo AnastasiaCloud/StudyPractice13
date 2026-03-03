a58cefd2b49306e505f63aa3d907436afa305e1f
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-03 15:46:47 +0500

    Использование агрегатной функции MAX

--- File1.py
+++ File1.py
@@ -3,8 +3,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT MIN(age) FROM Users')
-min_age = cursor.fetchone()[0]
-print('Минимальный возраст среди пользователей:', min_age)
+cursor.execute('SELECT MAX(age) FROM Users')
+max_age = cursor.fetchone()[0]
+print('Максимальный возраст среди пользователей:', max_age)
     
 connection.close()
