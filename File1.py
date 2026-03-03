f6f1b9f22ec7b2d8f95279183b57deb4ae3935c6
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-03 14:34:42 +0500

    Использование UPDATE

--- File1.py
+++ File1.py
@@ -15,5 +15,7 @@ cursor.execute('CREATE INDEX idx_email ON Users (email)')
 
 cursor.execute('INSERT INTO Users (username, email, age) VALUES (?, ?, ?)', ('newuser', 'newuser@example.com', 28))
 
+cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'newuser'))
+
 connection.commit()
 connection.close()
