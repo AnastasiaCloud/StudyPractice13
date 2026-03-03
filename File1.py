4f51ab875b847d4308e14a0b1bbb7d8078b877af
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-03 16:23:32 +0500

    Автоматическое управление транзакциями

--- File1.py
+++ File1.py
@@ -1,15 +1,13 @@
 import sqlite3
 
-connection = sqlite3.connect('my_database.db')
-cursor = connection.cursor()
+with sqlite3.connect('my_database.db') as connection:
+    cursor = connection.cursor()
 
-try:
-    cursor.execute('BEGIN')
-    cursor.execute('INSERT INTO Users (username, email) VALUES (?,?)', ('user1', 'user1@example.com'))
-    cursor.execute('INSERT INTO Users (username, email) VALUES (?,?)', ('user2', 'user2@example.com'))
+    try:
+        with connection:
+        cursor.execute('INSERT INTO Users (username, email) VALUES (?,?)', ('user3', 'user3@example.com'))
+        cursor.execute('INSERT INTO Users (username, email) VALUES (?,?)', ('user4', 'user4@example.com'))
 
-    cursor.execute('COMMIT')
-except:
-    cursor.execute('ROLLBACK')
+    except:
+        pass
 
-connection.close()
