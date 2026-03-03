43a3cd135d527b2003cdfe8aee438e7e9b76823b
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-03 15:49:52 +0500

    Пример сложного запроса с объединением таблиц и подтаблиц

--- File1.py
+++ File1.py
@@ -3,8 +3,13 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('SELECT MAX(age) FROM Users')
-max_age = cursor.fetchone()[0]
-print('Максимальный возраст среди пользователей:', max_age)
+cursor.execute('''
+SELECT username, age
+FROM Users
+WHERE age = (SELECT MAX(age) FROM Users)
+''')
+oldest_users = cursor.fetchall()
+for user in oldest_users:
+    print(user)
     
 connection.close()
