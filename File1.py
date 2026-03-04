3cd9b72861fa5dd282b1ecea82137f62a41cb557
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-04 12:37:44 +0500

    Работа с индексами для автоматизации запросов

--- File1.py
+++ File1.py
@@ -3,24 +3,8 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Deers (
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-''')
+cursor.execute('CREATE INDEX idx_username ON Deers(username)')
 
-cursor.execute('''
-CREATE TRIGGER IF NOT EXISTS update_created_at
-AFTER INSERT ON Deers
-BEGIN
-UPDATE Deers SET created_at = CURRENT_TIMESTAMP WHERE
-id = NEW.id;
-END;
-''')
 connection.commit()
 connection.close()
 
