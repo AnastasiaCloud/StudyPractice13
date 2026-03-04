664b07faf4cb23da6a317fc9603f532e1756cd43
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-04 12:35:38 +0500

    Создание триггеров для автоматизации операций при изменении данных

--- File1.py
+++ File1.py
@@ -3,12 +3,24 @@ import sqlite3
 connection = sqlite3.connect('my_database.db')
 cursor = connection.cursor()
 
-'''cursor.execute('CREATE VIEW ActiveUsers AS SELECT * FROM Users WHERE is_active = 1')'''
-cursor.execute('SELECT * FROM ActiveUsers')
-active_users = cursor.fetchall()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Deers (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+''')
 
-for user in active_users:
-    print(user)
-
+cursor.execute('''
+CREATE TRIGGER IF NOT EXISTS update_created_at
+AFTER INSERT ON Deers
+BEGIN
+UPDATE Deers SET created_at = CURRENT_TIMESTAMP WHERE
+id = NEW.id;
+END;
+''')
+connection.commit()
 connection.close()
 
