77f945fc4ef9890282f45b84bd759ad90211ebab
AnastasiaCloud <ngoleva58@gmail.com>
2026-03-06 21:21:28 +0500

    Подключение всех окон и форм. Выполнение индивидуального задания.

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ class main_window(QMainWindow):
             icon = QIcon(icon_path)
             item.setIcon(icon)
             self.ui.tableWidget.setItem(row, 2, item)
-            self.ui.tableWidget.setIconSize(QSize(70, 70))
+            self.ui.tableWidget.setIconSize(QSize(100, 100))
 
         self.ui.tableWidget.resizeColumnsToContents()
         self.ui.tableWidget.resizeRowsToContents()
@@ -94,7 +94,7 @@ class main_window(QMainWindow):
             icon = QIcon(icon_path)
             item.setIcon(icon)
             self.ui.tableWidget.setItem(row, 1, item)
-            self.ui.tableWidget.setIconSize(QSize(70, 70))
+            self.ui.tableWidget.setIconSize(QSize(100, 100))
 
 
         self.ui.tableWidget.resizeColumnsToContents()
@@ -104,21 +104,21 @@ class main_window(QMainWindow):
         print()
         self.ui.tableWidget.setRowCount(0)
         cursor.execute('SELECT * FROM Заказы')
-        self.medicine_data = cursor.fetchall()
+        self.orders_data = cursor.fetchall()
 
         self.ui.tableWidget.setColumnCount(3)
         self.ui.tableWidget.setHorizontalHeaderLabels(['Заказы', 'Скидка'])
 
-        self.ui.tableWidget.setRowCount(len(self.medicine_data))
-        for row in range(len(self.medicine_data)):
-            text = (str(self.medicine_data[row][0]) + ' | ' + str(self.medicine_data[row][1])
-                    + ' | ' + str(self.medicine_data[row][2])
-                    + '\n' + str(self.medicine_data[row][3]) + '\nКоличество: '
-                    + str(self.medicine_data[row][4]))
+        self.ui.tableWidget.setRowCount(len(self.orders_data))
+        for row in range(len(self.orders_data)):
+            text = (str(self.orders_data[row][0]) + ' | ' + str(self.orders_data[row][1])
+                    + ' | ' + str(self.orders_data[row][2])
+                    + '\n' + str(self.orders_data[row][3]) + '\nКоличество: '
+                    + str(self.orders_data[row][4]))
             item = QTableWidgetItem()
             item.setText(text)
             self.ui.tableWidget.setItem(row, 0, item)
-            price = str(self.medicine_data[row][5])
+            price = str(self.orders_data[row][5])
             item = QTableWidgetItem()
             item.setText(price)
             self.ui.tableWidget.setItem(row, 1, item)
@@ -127,23 +127,82 @@ class main_window(QMainWindow):
         self.ui.tableWidget.resizeRowsToContents()
 
     def open_add_form(self):
-        self.client_form = client_window(self)
-        self.client_form.ui.pushButton.clicked.connect(self.client_form.create_client)
-        self.client_form.exec()
+        current_table = self.ui.comboBox.currentText()
+        
+        if current_table == 'Оптовые_клиенты':
+            self.client_form = client_window(self)
+            self.client_form.ui.pushButton.clicked.connect(self.client_form.create_client)
+            self.client_form.exec()
+        elif current_table == 'Препараты':
+            self.medicine_form = medicine_window(self)
+            self.medicine_form.ui.pushButton.clicked.connect(self.medicine_form.create_medicine)
+            self.medicine_form.exec()
+        elif current_table == 'Заказы':
+            self.orders_form = orders_window(self)
+            self.orders_form.ui.pushButton.clicked.connect(self.orders_form.create_order)
+            # Заполняем комбобоксы данными из связанных таблиц
+            self.orders_form.load_combo_boxes()
+            self.orders_form.exec()
 
     def open_update_form(self):
-        self.client_form = client_window(self)
-        client_data = self.client_data[self.ui.tableWidget.currentRow()]
-        self.client_form.inn = client_data[0]
+        current_table = self.ui.comboBox.currentText()
+        current_row = self.ui.tableWidget.currentRow()
+        
+        if current_row < 0:
+            return
+            
+        if current_table == 'Оптовые_клиенты':
+            self.client_form = client_window(self)
+            if hasattr(self, 'client_data') and current_row < len(self.client_data):
+                client_data = self.client_data[current_row]
+                self.client_form.inn = client_data[0]
+                
+                # Заполняем поля формы
+                self.client_form.ui.lineEdit.setText(str(client_data[0]))  
+                self.client_form.ui.lineEdit_2.setText(str(client_data[1]))  
+                self.client_form.ui.lineEdit_3.setText(str(client_data[2]))  
+                self.client_form.ui.lineEdit_4.setText(str(client_data[3]))  
+                self.client_form.ui.lineEdit_5.setText(str(client_data[4]))  
 
-        self.client_form.ui.lineEdit.setText(str(client_data[0]))
-        self.client_form.ui.lineEdit_2.setText(str(client_data[1]))
-        self.client_form.ui.lineEdit_3.setText(str(client_data[2]))
-        self.client_form.ui.lineEdit_4.setText(str(client_data[3]))
-        self.client_form.ui.lineEdit_5.setText(str(client_data[4]))
+                self.client_form.ui.pushButton.clicked.connect(self.client_form.update_client)
+                self.client_form.exec()
+                
+        elif current_table == 'Препараты':
+            self.medicine_form = medicine_window(self)
+            if hasattr(self, 'medicine_data') and current_row < len(self.medicine_data):
+                medicine_data = self.medicine_data[current_row]
+                self.medicine_form.inn = medicine_data[0]
+                
+                # Заполняем поля формы
+                self.medicine_form.ui.lineEdit.setText(str(medicine_data[0]))   
+                self.medicine_form.ui.lineEdit_2.setText(str(medicine_data[1])) 
+                self.medicine_form.ui.lineEdit_3.setText(str(medicine_data[2])) 
+                self.medicine_form.ui.lineEdit_4.setText(str(medicine_data[3])) 
+                self.medicine_form.ui.lineEdit_5.setText(str(medicine_data[4])) 
+                self.medicine_form.ui.lineEdit_6.setText(str(medicine_data[5])) 
 
-        self.client_form.ui.pushButton.clicked.connect(self.client_form.update_client)
-        self.client_form.exec()
+                self.medicine_form.ui.pushButton.clicked.connect(self.medicine_form.update_medicine)
+                self.medicine_form.exec()
+                
+        elif current_table == 'Заказы':
+            self.orders_form = orders_window(self)
+            if hasattr(self, 'orders_data') and current_row < len(self.orders_data):
+                orders_data = self.orders_data[current_row]
+                self.orders_form.inn = orders_data[0]
+                
+                # Загружаем данные для комбобоксов
+                self.orders_form.load_combo_boxes()
+                
+                # Заполняем поля формы
+                self.orders_form.ui.comboBox.setCurrentIndex(self.orders_form.find_combo_index_by_client_id(orders_data[1]))
+                self.orders_form.ui.comboBox_2.setCurrentIndex(self.orders_form.find_combo_index_by_medicine_id(orders_data[2]))
+                self.orders_form.ui.dateEdit.setDate(QDate.fromString(orders_data[3], 'yyyy-MM-dd'))
+                self.orders_form.ui.lineEdit.setText(str(orders_data[4]))  # Количество
+                self.orders_form.ui.lineEdit_2.setText(str(orders_data[5]))  # Скидка
+
+                self.orders_form.ui.pushButton.clicked.connect(self.orders_form.update_order)
+                self.orders_form.exec()
+
 
 class client_window(QDialog):
     def __init__(self, parent=None):
@@ -248,13 +307,46 @@ class orders_window(QDialog):
         self.ui = ordersUI()
         self.ui.setupUi(self)
 
+        self.client_data = []
+        self.medicine_data = []
+
+    def load_combo_boxes(self):
+        # Загружаем список клиентов
+        cursor.execute('SELECT client_number, surname, name FROM Оптовые_клиенты')
+        self.client_data = cursor.fetchall()
+        self.ui.comboBox.clear()
+        for client in self.client_data:
+            self.ui.comboBox.addItem(f"{client[1]} {client[2]} (ID: {client[0]})", client[0])
+        
+        # Загружаем список препаратов
+        cursor.execute('SELECT medicine_number, name FROM Препараты')
+        self.medicine_data = cursor.fetchall()
+        self.ui.comboBox_2.clear()
+        for medicine in self.medicine_data:
+            self.ui.comboBox_2.addItem(f"{medicine[1]} (ID: {medicine[0]})", medicine[0])
+        
+        # Устанавливаем текущую дату
+        self.ui.dateEdit.setDate(QDate.currentDate())
+
+    def find_combo_index_by_client_id(self, client_id):
+        for i in range(self.ui.comboBox.count()):
+            if self.ui.comboBox.itemData(i) == client_id:
+                return i
+        return 0
+
+    def find_combo_index_by_medicine_id(self, medicine_id):
+        for i in range(self.ui.comboBox_2.count()):
+            if self.ui.comboBox_2.itemData(i) == medicine_id:
+                return i
+        return 0
+
     def create_order(self):
         client_data = [
+                       self.ui.comboBox.currentData(),
+                       self.ui.comboBox_2.currentData(),
+                       self.ui.dateEdit.text().toString('yyyy-MM-dd'),
                        self.ui.lineEdit_2.text(),
-                       self.ui.lineEdit_3.text(),
-                       self.ui.lineEdit_4.text(),
-                       self.ui.lineEdit_5.text(),
-                       self.ui.lineEdit_6.text()]
+                       self.ui.lineEdit_3.text()]
         if any([item == '' for item in client_data]):
             QMessageBox.critical(self, 'Not done', 'Fill all fields', QMessageBox.Ok)
             return
@@ -272,11 +364,11 @@ class orders_window(QDialog):
     def update_order(self):
         order_id = self.inn
         order_data = [
+                       self.ui.comboBox.currentData(),
+                       self.ui.comboBox_2.currentData(),
+                       self.ui.dateEdit.text().toString('yyyy-MM-dd'),
                        self.ui.lineEdit_2.text(),
-                       self.ui.lineEdit_3.text(),
-                       self.ui.lineEdit_4.text(),
-                       self.ui.lineEdit_5.text(),
-                       self.ui.lineEdit_6.text()]
+                       self.ui.lineEdit_3.text()]
         if any([item == '' for item in client_data]):
             QMessageBox.critical(self, 'Not done', 'Fill all fields', QMessageBox.Ok)
             return
@@ -296,6 +388,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)  
     conn = sqlite3.connect('Микстура.db')  
     cursor = conn.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
     main_form = main_window()  
     main_form.show()  
     sys.exit(app.exec_())  
